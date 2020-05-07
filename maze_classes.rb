12ff135a3650c1ed7fdb14fd3b7fd3af8c46fbd4
Selt0 <chewedgum12@gmail.com>
2020-05-06 21:41:26 -0500

    Add methods to read a map from file

--- maze_classes.rb
+++ maze_classes.rb
@@ -1,8 +1,47 @@
 module MazeClasses
 
   class Maze
+    DELTAS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-    def initialize
+    attr_reader :start_idx, :end_idx, :map, :title
+    def initialize(filename)
+      @map = load_map(filename)
+      @title = parse_title(filename)
+      @start_idx = find_start
+      @end_idx = find_end
+    end
+
+    def load_map(filename)
+      maze = []
+      File.open(filename).each_line do |line|
+        chars = line.split(//)
+        maze << chars
+      end
+      maze
+    end
+
+    def parse_title(filename)
+      filename.match(/(.+)\.txt/)[1]
+    end
+
+    def find_start
+      find_char('S')
+    end
+
+    def find_end
+      find_char('E')
+    end
+
+    def find_char(char)
+      map.each_with_index do |line, y|
+        return [line.index(char), y] if line.index(char)
+      end
+    end
+
+    def to_s
+      string = "MAZE: #{title}\n"
+      map.each { |line| string << line.join('') }
+      string
     end
   end
 
